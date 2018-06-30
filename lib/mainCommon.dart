b22df7a8e239b8e1485bb108736a405faa78428c
vjrj <vjrj@ourproject.org>
2018-06-30 08:17:45 +0200

    YourLocations persist refactor

--- lib/mainCommon.dart
+++ lib/mainCommon.dart
@@ -1,42 +1,39 @@
 import 'dart:async';
 
 import 'package:comunes_flutter/comunes_flutter.dart';
+import 'package:fires_flutter/models/yourLocationPersist.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_simple_dependency_injection/injector.dart';
 import 'package:redux/redux.dart';
 
 import 'firebaseMessagingConf.dart';
 import 'firesApp.dart';
-import 'globals.dart' as globals;
 import 'models/appState.dart';
 import 'models/firesApi.dart';
 import 'redux/fetchDataMiddleware.dart';
 import 'redux/reducers.dart';
-import 'package:fires_flutter/models/yourLocationPersist.dart';
-import 'package:flutter_simple_dependency_injection/injector.dart';
 
 Future<Map<String, dynamic>> loadSecrets() async {
   return await SecretLoader(secretPath: 'assets/private-settings.json').load();
 }
 
 void mainCommon(List<Middleware<AppState>> otherMiddleware) {
-
   final injector = Injector.getInjector();
   injector.map(FiresApi, (i) => new FiresApi(), isSingleton: true);
   loadSecrets().then((secrets) {
-
     final store = new Store<AppState>(appStateReducer,
         initialState: new AppState(
             gmapKey: secrets['gmapKey'],
             firesApiKey: secrets['firesApiKey'],
             firesApiUrl: secrets['firesApiUrl'] + "api/v1/"),
-        middleware: List.from(otherMiddleware)..add(fetchYourLocationsMiddleware));
+        middleware: List.from(otherMiddleware)
+          ..add(fetchYourLocationsMiddleware));
 
     injector.map(String, (i) => store.state.firesApiUrl, key: "firesApiUrl");
     injector.map(String, (i) => store.state.firesApiKey, key: "firesApiKey");
     injector.map(String, (i) => store.state.gmapKey, key: "gmapKey");
 
-    globals.prefs.then((prefs) {
-      loadYourLocationsWithPrefs(prefs);
+    loadYourLocations().then((yl) {
       firebaseConfig(store);
 
       // Run baby run!
@@ -46,7 +43,5 @@ void mainCommon(List<Middleware<AppState>> otherMiddleware) {
     // Listen to store changes, and re-render when the state is updated
     // store.onChange.listen((state) {
     // });
-
   });
-
 }
--- lib/models/yourLocationPersist.dart
+++ lib/models/yourLocationPersist.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:fires_flutter/models/yourLocation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../globals.dart' as globals;
 
@@ -10,33 +9,27 @@ final String locationKey = 'yourlocations';
 
 Future<List<YourLocation>> loadYourLocations() async {
   return await globals.prefs.then((prefs) {
-    return loadYourLocationsWithPrefs(prefs);
-  });
-}
-
-persistYourLocations(List<YourLocation> yl) {
-  globals.prefs.then((prefs) {
-    List<String> yourLocationsAsString = [];
-    yl.forEach((location) {
-      yourLocationsAsString.add(json.encode(location.toJson()));
-    });
-    prefs.setStringList(locationKey, yourLocationsAsString);
-  });
-}
-
-Future<List<YourLocation>> loadYourLocationsWithPrefs(
-    SharedPreferences prefs) async {
-  return await globals.prefs.then((prefs) {
     List<String> yourLocations = prefs.getStringList(locationKey);
+    List<YourLocation> persistedList = List<YourLocation>();
     if (yourLocations == null) {
       yourLocations = [];
-      persistYourLocations(<YourLocation>[]);
+      // first run, init with empty list
+      persistYourLocations(persistedList);
     }
-    List<YourLocation> persistedList = List<YourLocation>();
     yourLocations.forEach((locationString) {
       Map locationMap = json.decode(locationString);
       persistedList.add(YourLocation.fromJson(locationMap));
     });
     return persistedList;
+  });
+}
+
+persistYourLocations(List<YourLocation> yl) {
+  globals.prefs.then((prefs) {
+    List<String> ylAsString = [];
+    yl.forEach((location) {
+      ylAsString.add(json.encode(location.toJson()));
+    });
+    prefs.setStringList(locationKey, ylAsString);
   });
 }
